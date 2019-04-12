6442e8f22e48748da34579c995f8b7412d31207c
Kim <kim99602@naver.com>
2019-04-12 10:33:40 +0900

    2

--- OSS_Experiments06/src.cpp
+++ OSS_Experiments06/src.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-	for (int i = 1; i <= 8; ++i) {
+	for (int i = 1; i <= 9; ++i) {
 		for (int j = 1; j <= 9; ++j) {
 			printf_s("%d x %d = %d\n", i, j, i * j);
 		}
